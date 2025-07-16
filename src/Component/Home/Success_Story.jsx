7713f545d55730a5a9915c8604a098bbf349e57c
Tanvir225 <tanvirrehman200@gmail.com>
2025-07-16 13:09:48 +0600

    success_story has been added

---
+++ src/Component/Home/Success_Story.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Success_Story = () => {
+    return (
+        <div className='border-2 border-teal-200 rounded-2xl p-2 bg-gray-100 dark:bg-gray-800'>
+            <div className="max-w-[300px] space-y-8 rounded-2xl px-6 py-8 shadow-md  dark:bg-[#18181B] pt-16">
+                {/* profile image & bg  */}
+                <div className="relative">
+                    <img
+                        width={100}
+                        height={100}
+                        className="absolute -bottom-12 left-1/2 h-[100px] w-[100px] -translate-x-1/2 rounded-full border-4 border-white bg-gray-400 dark:border-[#18181B]"
+                        src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=2080&auto=format&fit=crop"
+                        alt="card navigate ui"
+                    />
+                </div>
+                {/* profile name & role */}
+                <div className="space-y-1 pt-8 text-center">
+                    <h1 className="text-xl md:text-2xl">Shiyam Sarker</h1>
+                    <p className="text-sm text-gray-400">Wordpress Web Development</p>
+                </div>
+                {/* profile description */}
+                <div className="space-y-4 text-center">
+                    <p className="text-gray-500 dark:text-gray-400">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, voluptatum
+                        cumque. Quisquam, asperiores. Doloribus, voluptatum cumque. Quisquam, asperiores.
+                    </p>
+                </div>
+
+
+            </div>
+        </div>
+    );
+};
+
+export default Success_Story;
--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -6,6 +6,8 @@ import Course from '../../Component/Share/Course';
 import Timeline from '../../Component/Home/Timeline';
 import FAQ from '../../Component/Share/FAQ';
 import contest from '../../assets/contest.gif';
+import Success_Story from '../../Component/Home/Success_Story';
+import Marquee from "react-fast-marquee";
 
 const Home = () => {
     return (
@@ -39,11 +41,25 @@ const Home = () => {
                 <Title title="Become_ A _ Champion" subtitle={<>Join <span className='bg-teal-200 p-1 rounded-xl text-black'>Contest</span> & Get <span className='bg-teal-200 p-1 rounded-xl text-black'>Money</span> </>}></Title>
 
                 <div className='border-2 mt-4 border-teal-200 lg:h-[500px]  rounded-2xl'>
-                    <img className='w-full h-full' src={contest} alt=""/>
+                    <img className='w-full h-full' src={contest} alt="" />
                 </div>
             </section>
             <section className='my-16'>
                 <Title title="Success_Story"></Title>
+
+                <Marquee className='my-10' speed={50} gradient={true} pauseOnHover={true}>
+
+                    <div className='flex items-center justify-center gap-10 rounded-xl'>
+                        <Success_Story></Success_Story>
+                        <Success_Story></Success_Story>
+                        <Success_Story></Success_Story>
+                        <Success_Story></Success_Story>
+                    </div>
+
+                </Marquee>
+
+
+
             </section>
             <section className='my-16'>
                 <Title title="FAQ"></Title>
