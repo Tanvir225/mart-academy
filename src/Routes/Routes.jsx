7c8b9c7f55181313e38ef36bb728aa9f463b8ad3
Tanvir225 <tanvirrehman200@gmail.com>
2025-07-23 13:38:30 +0600

    sign in page design has been added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../Page/Home/Home";
 import Main from "../Layout/Main";
+import Login from "../Page/Login/Login";
 
 
 const router = createBrowserRouter([
@@ -12,9 +13,20 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home></Home>,
       },
-   
+
     ],
+
+
+
   },
+
+  {
+    //login
+    path: "/login",
+    element: <Login></Login>,
+  }
+
+
 ]);
 
 export default router;
