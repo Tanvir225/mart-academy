63a9eb4cb7bb1c2d36ce07e3b9ca6b117ed85dd4
Tanvir225 <tanvirrehman200@gmail.com>
2025-08-02 11:15:15 +0600

    navbar problem fix

--- src/Component/Share/Navbar.jsx
+++ src/Component/Share/Navbar.jsx
@@ -73,7 +73,7 @@ const Navbar = () => {
                 </div>
             </div>
 
-            <div className="flex space-x-2 lg:space-x-3">
+            <div className="flex space-x-2 lg:space-x-3 relative z-20">
                 {
                     user ? (
                         <div className="dropdown dropdown-end">
@@ -86,7 +86,7 @@ const Navbar = () => {
                             </div>
                             <ul
                                 tabIndex={0}
-                                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow">
+                                className="menu menu-sm dropdown-content bg-base-100 rounded-box  mt-3 w-52 p-2 shadow">
                                 <li>
                                     <a className="justify-between">
                                         {user?.displayName || "Profile"}
