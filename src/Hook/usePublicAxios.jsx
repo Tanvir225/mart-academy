aab095062677b8777184e40b0cfc2b09c54f8947
Tanvir225 <tanvirrehman200@gmail.com>
2025-08-14 16:04:09 +0600

    web icon has been added

--- src/Hook/usePublicAxios.jsx
+++ src/Hook/usePublicAxios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:5173",
+    baseURL: import.meta.env.VITE_PRIMARY_API_URL || "http://localhost:5173",
     withCredentials: true,
 });
 
