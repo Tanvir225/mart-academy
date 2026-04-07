e198b5fb9b3adbb48d2e27bb6751663d3f731cf4
Tanvir225 <tanvirrehman200@gmail.com>
2026-04-07 13:31:29 +0600

    my course get api and ui has been added

--- src/Page/Courses/Courses.jsx
+++ src/Page/Courses/Courses.jsx
@@ -1,4 +1,4 @@
-import { use, useEffect, useState } from "react";
+import {useEffect, useState } from "react";
 import useUser from "../../Hook/useUser";
 import CourseCard from "./CourseCard";
 import useAxios from "../../Hook/useAxios";
